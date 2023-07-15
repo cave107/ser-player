80a87f6a8dc43c9b8925837d02fba41f8daea9b7
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2023-07-15 18:28:36 +0200

    Fixed macdeployqt and temporarily disabled

--- platform-specific/os-x/post_compile.sh
+++ platform-specific/os-x/post_compile.sh
@@ -1,4 +1,5 @@
 echo Running Mac post compile script
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cp $SCRIPT_DIR/info.plist $SCRIPT_DIR/../../bin/ser-player.app/Contents/
-$QTDIR/bin/macdeployqt $SCRIPT_DIR/../../bin/SER-Player.app -dmg
+#$QTDIR/bin/macdeployqt $SCRIPT_DIR/../../bin/SER-Player.app -dmg
+#env macdeployqt $SCRIPT_DIR/../../bin/SER-Player.app -dmg
